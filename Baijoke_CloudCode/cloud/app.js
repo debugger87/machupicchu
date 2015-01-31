831438d2c21f917efec964d2598e62b32e5d305d
WuJun <jun.wu@avoscloud.com>
2015-01-31 14:23:21 +0800

    add static html sample.

--- Baijoke_CloudCode/cloud/app.js
+++ Baijoke_CloudCode/cloud/app.js
@@ -6,11 +6,13 @@ var app = express();
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
-
+app.use(express.static('public'));
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
-
+app.get('/test', function(req, res) {
+  res.render('liyanhong');
+});
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
