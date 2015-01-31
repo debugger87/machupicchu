a73dc05e91f5e664febd879fa8150da46d426a7f
debugger87 <yangchaozhong.2009@gmail.com>
2015-01-31 19:48:20 +0800

    first version of crawler

--- Baijoke_CloudCode/cloud/main.js
+++ Baijoke_CloudCode/cloud/main.js
@@ -1,6 +1,47 @@
 require("cloud/app.js");
-// Use AV.Cloud.define to define as many cloud functions as you want.
-// For example:
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+
+var crawler = require("crawler");
+var _ = require('underscore');
+
+var c = new crawler({
+  maxConnections : 10,
+  forceUTF8: true,
+  jQuery: jsdom,
+
+  // This will be called for each crawled page
+  callback : function (error, result, $) {
+    // $ is Cheerio by default
+
+    var template = AV.Object.extend("template");
+    var t = new template();
+
+    $('.card-summary-content').each(function(index, summary) {
+      t.set("summary", $(summary).html());
+    });
+
+    var relatedPersons = [];
+    $('#zhixinWrap a').each(function(index, a) {
+      relatedPersons.push($(a).attr("href"));
+    });
+    t.set("relatedPersons", relatedPersons);
+
+    var tags = [];
+    $('.taglist').each(function(index, taglist){
+      tags.push($(taglist).html());
+    });
+
+    t.set("tags", tags);
+    t.save();
+  }
+});
+
+AV.Cloud.define("crawler", function(request, response) {
+  console.log("crawling baike views...");
+  var query = new AV.Query("template")
+  query.find().then(function(templates) {
+    _.each(templates, function(t) {
+      var baikeUrl = t.get("baikeUrl");
+      c.queue(baikeUrl);
+    });
+  });
 });
